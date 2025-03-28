7bc00b9359dfe9279ec465da20a511f18a6bf85f
ThomasCorrea-Dev <thomas.correa@aluno.senai.br>
2025-03-28 10:33:58 -0300

    Dom-01

---
+++ Script-2.js
@@ -0,0 +1,14 @@
+let titulo = document.getElementById("titulo");
+console.log(titulo);
+
+let Esporte = document.getElementsByClassName("Esporte");
+console.log(Esporte);
+
+let EsportesTag = document.getElementsByTagName("ul");
+console.log(EsportesTag[0]);
+
+let EsporteQS = document.querySelector(".Esporte");
+console.log(EsporteQS.textContent);
+
+let EsporteQSall = document.querySelectorAll(".Esporte");
+console.log(EsporteQSall);
